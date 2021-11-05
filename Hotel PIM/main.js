a81f9fc7f5a448ba50ca3b4869a643ec7e207ff8
Menesesgabriel <89167544+Menesesgabriel@users.noreply.github.com>
2021-11-05 15:45:14 -0300

    Add files via upload

---
+++ Hotel PIM/main.js
@@ -0,0 +1,7 @@
+//Seletores
+let header = document.querySelector('.header');
+let hamburguerMenu = document.querySelector('.hamburguer-menu');
+
+hamburguerMenu.addEventListener('click', function(){
+    header.classList.toggle('menu-open');
+})
